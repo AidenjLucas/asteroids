24f7597cc1643b3f43986277929d5387d63340b6
aidenjlucas <aidenjlucas.work@gmail.com>
2025-05-05 12:53:03 -0400

    Updated Constants,Main,Player

--- main.py
+++ main.py
@@ -4,32 +4,37 @@
 # testing git
 import pygame
 from constants import *
-import player
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    play = player.Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
-    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
        
         screen.fill("black")
-        play.update(dt)
-        play.draw(screen)
 
+        updatable.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
-        
-        
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-
 
 
 if __name__ == "__main__":
