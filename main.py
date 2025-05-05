c76733fb9794bb832258de7f786b2bcdc4386fa2
aidenjlucas <aidenjlucas.work@gmail.com>
2025-05-05 12:03:55 -0400

    added Player Class, added CircleShape class, limited framerate.

--- main.py
+++ main.py
@@ -4,19 +4,30 @@
 # testing git
 import pygame
 from constants import *
+import player
+
 def main():
-   
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    play = player.Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    dt = 0
 
     while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+       
         screen.fill("black")
 
+        play.draw(screen)
 
         pygame.display.flip()
+
+        
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 
