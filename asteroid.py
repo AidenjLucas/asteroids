878bb25c6964b24aa18adad84facfca4f3788388
aidenjlucas <aidenjlucas.work@gmail.com>
2025-05-06 09:48:26 -0400

    Added Screen Wrapping for Asteroids
    Added ASTEROID_SPAWN_PROTECTION to constants.py
    Added spawn_protection variable to asteroid class to prevent jittering when spawning due to wrapping logic

--- asteroid.py
+++ asteroid.py
@@ -4,15 +4,31 @@ from circleshape import CircleShape
 from constants import *
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius,spawn_protection):
         super().__init__(x, y, radius)
-
+        self.spawn_protection = spawn_protection
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
+        
         self.position += self.velocity * dt
-    
+
+        if self.spawn_protection < 0:
+        # Step 2: wrap on X axis
+            if self.position.x > SCREEN_WIDTH:
+                self.position.x = 0
+            elif self.position.x < 0:
+                self.position.x = SCREEN_WIDTH
+
+        # Step 3: wrap on Y axis
+            if self.position.y > SCREEN_HEIGHT:
+                self.position.y = 0
+            elif self.position.y < 0:
+                self.position.y = SCREEN_HEIGHT
+           
+        self.spawn_protection -= 0.1
+
     def split(self):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
@@ -24,8 +40,8 @@ class Asteroid(CircleShape):
         velocity2 = self.velocity.rotate(-angle)
         new_radius = self.radius - ASTEROID_MIN_RADIUS
 
-        asteroid1 = Asteroid(self.position.x,self.position.y,new_radius)
-        asteroid2 = Asteroid(self.position.x,self.position.y,new_radius)
+        asteroid1 = Asteroid(self.position.x,self.position.y,new_radius,0)
+        asteroid2 = Asteroid(self.position.x,self.position.y,new_radius,0)
 
         asteroid1.velocity = velocity1 * 1.2
         asteroid2.velocity = velocity2 * 1.2
